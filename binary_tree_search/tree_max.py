842d027b077f727fc37b9327ec6632c8e9d61c4f
Biruk Maru <birukyoni12@gmail.com>
2024-11-01 13:52:33 +0300

    tree_max

---
+++ binary_tree_search/tree_max.py
@@ -0,0 +1,7 @@
+def treeMax(root):
+    if root is None:
+        return float("-inf")
+    else:
+        leftMax=treeMax(root.left)
+        rightMax=treeMax(root.right)
+        return max(root.data,leftMax,rightMax)
