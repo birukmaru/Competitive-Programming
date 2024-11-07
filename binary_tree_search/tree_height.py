55d1846b9eeebcd119cd9b93bd8aa2e32a767114
Biruk Maru <birukyoni12@gmail.com>
2024-11-07 09:28:03 +0300

    add tree height

---
+++ binary_tree_search/tree_height.py
@@ -0,0 +1,8 @@
+def treeHeight(root):
+    if root is None:
+        return 0
+    else:
+        leftHeight=treeHeight(root.left)
+        rightHeight=treeHeight(root.right)
+        return 1 + max(leftHeight,rightHeight)
+    
