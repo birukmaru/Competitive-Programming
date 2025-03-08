c1da68f49ddda850be5e5d037e2e988e3958355e
Biruk Maru <birukyoni12@gmail.com>
2025-03-08 23:20:47 +0300

    make some fixes

--- prefix_sum/maximus_side_length.py
+++ prefix_sum/maximus_side_length.py
@@ -6,19 +6,15 @@ class Solution:
         prefix[i][j] = prefix[i-1][j] + prefix[i][j-1] - prefix[i-1][j-1] + mat[i][j]
         """
         m, n = len(mat), len(mat[0])
-        #build prefix sum 
         prefix = [[0]*(n+1) for _ in range(m+1)]
         
         for i, j in product(range(m), range(n)):
             prefix[i+1][j+1] = prefix[i+1][j] + prefix[i][j+1] - prefix[i][j] + mat[i][j]
         
         def below(k): 
-            """reture true if there is such a sub-matrix of length k"""
             for i, j in product(range(m+1-k), range(n+1-k)):
                 if prefix[i+k][j+k] - prefix[i][j+k] - prefix[i+k][j] + prefix[i][j] <= threshold: return True
             return False 
-                
-        #binary search
         lo, hi = 1, min(m, n)
         while lo <= hi: 
             mid = (lo + hi)//2
