87db74bae22f17fa8db12e132860b7f4ad5d6b93
Biruk Maru <birukyoni12@gmail.com>
2025-03-12 20:01:55 +0300

    long pressed two pointer

---
+++ two_pointer/long_pressed.py
@@ -0,0 +1,13 @@
+class Solution:
+    def isLongPressedName(self, name, typed) -> bool:
+        i=0
+        for j in range(len(typed)):
+            if i<len(name) and typed[j]==name[i]:
+                i += 1
+            elif j==0 or typed[j] != typed[j-1]:
+                return False
+        return i==len(name)
+
+            
+
+        
